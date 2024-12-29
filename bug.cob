b5433641a6525dc901c802cbba7241c9f4dc4b58
BugHunter-X <askzenai@gmail.com>
2024-12-29 15:06:04 +0100

    Add bug.cob file

---
+++ bug.cob
@@ -0,0 +1,9 @@
+```cobol
+       01  WS-DATA-1 PIC 9(5) VALUE 12345.
+       01  WS-DATA-2 PIC 9(5) VALUE 67890.
+       01  WS-SUM PIC 9(6) VALUE ZEROES.
+
+       ADD WS-DATA-1 TO WS-SUM.
+       ADD WS-DATA-2 TO WS-SUM.
+       DISPLAY "Sum: " WS-SUM
+```
